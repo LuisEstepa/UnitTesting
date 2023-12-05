d9208b592c3dc61234dc41740bd1f4b9a7979844
LuisEstepa <116852447+LuisEstepa@users.noreply.github.com>
2023-12-05 13:55:09 -0500

    prueba polindromos

---
+++ StringManipulationTest/StringOperationsTest.cs
@@ -0,0 +1,43 @@
+﻿using StringManipulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulationTest
+{
+    public class StringOperationsTest
+    {
+        [Fact]
+        public void ConcatenateStrings()
+        {
+            //Arrange
+            var strOperations = new StringOperations();
+            //Act
+            var result = strOperations.ConcatenateStrings("Pedro", "Estepa");
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.Equal("Pedro Estepa", result);
+
+        }
+
+        [Fact]
+        public void IsPalindrome()
+        {
+            //Arrange
+            var strOperations = new StringOperations();
+            
+            //Act
+            var result = strOperations.IsPalindrome("ama");
+
+            //Assert            
+            
+            Assert.True(result);
+
+        }
+    }
+}
+
