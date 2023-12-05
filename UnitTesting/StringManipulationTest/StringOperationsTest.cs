11b906b490582d66d5af3ecb303cb11784d2a972
LuisEstepa <116852447+LuisEstepa@users.noreply.github.com>
2023-12-05 13:23:58 -0500

    primera prueba
    primera prueba

---
+++ UnitTesting/StringManipulationTest/StringOperationsTest.cs
@@ -0,0 +1,25 @@
+﻿using StringManipulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulationTest
+{
+    public class StringOperationsTest
+    {
+        [Fact] 
+        public void ConcatenateStrings() 
+        {
+            //Arrange
+            var strOperations = new StringOperations();
+            //Act
+            var result = strOperations.ConcatenateStrings("Pedro", "Estepa");
+            
+            //Assert
+            Assert.Equal("Pedro Estepa", result);
+
+        }
+    }
+}
